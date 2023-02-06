751991ac888a0628588b6c0d5fa0af16d03f06de
Andrei <andreiomega@hotmail.com>
2023-02-06 19:03:10 -0300

    data em js

--- js/date.js
+++ js/date.js
@@ -1,4 +1,59 @@
-const today = new Date();
-today.toLocaleDateString();
+const date = new Date();
+mes = date.getMonth();
+dia = date.getDate();
+ano = date.getFullYear();
 
-console.log(today);
+switch (mes) {
+	case 0:
+		mes = "Janeiro";
+		break;
+
+	case 1:
+		mes = "Fevereiro";
+		break;
+
+	case 2:
+		mes = "Março";
+		break;
+
+	case 3:
+		mes = "Abril";
+		break;
+
+	case 4:
+		mes = "Maio";
+		break;
+
+	case 5:
+		mes = "Junho";
+		break;
+
+	case 6:
+		mes = "Julho";
+		break;
+
+	case 7:
+		mes = "Agosto";
+		break;
+
+	case 8:
+		mes = "Setembro";
+		break;
+
+	case 9:
+		mes = "Outubro";
+		break;
+
+	case 10:
+		mes = "Novembro";
+		break;
+
+	default:
+		mes = "Dezembro";
+		break;
+}
+
+hoje = dia + " de " + mes + " de " + ano;
+// hoje = `${dia} de ${mes} de ${ano} `;
+
+document.getElementById("data").innerHTML = hoje;
